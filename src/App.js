47c26fe7cc1ea947e651546d7b75d6b88924efca
AntoineGGG <a.gorenflot@gmx.fr>
2020-10-12 17:50:46 +0200

    Creation Chat plusieurs

--- src/App.js
+++ src/App.js
@@ -4,7 +4,24 @@ import Contact from './components/Contact';
 function App() {
   return (
     <div className='App'>
-      <Contact />
+      <Contact
+        img='https://randomuser.me/api/portraits/women/26.jpg'
+        name='Tina Palmer'
+        status='Online'
+        statusText='Online'
+      />
+      <Contact
+        img='https://randomuser.me/api/portraits/men/88.jpg'
+        name='Ken Wheeler'
+        status=''
+        statusText='Offline'
+      />
+      <Contact
+        img='https://randomuser.me/api/portraits/men/14.jpg'
+        name='Lee Walker'
+        status=''
+        statusText='Offline'
+      />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import './Contact.css';
+import propTypes from 'prop-types';
 
-function Contact() {
+function Contact(props) {
   return (
     <div className='Contact'>
-      <img
-        className='avatar'
-        src='https://randomuser.me/api/portraits/women/38.jpg'
-      />
+      <img className='avatar' src={props.img} />
       <div>
-        <p className='name'>Audrey Machin</p>
+        <p className='name'> {props.name}</p>
         <div className='status'>
-          <div className='status-online'></div>
-          <p className='status-text'>Online</p>
+          <div
+            className={props.status ? 'status-online' : 'status-offline'}
+          ></div>
+          <p className='status-text'>{props.statusText}</p>
         </div>
       </div>
     </div>
   );
 }
-
+Contact.propTypes = {
+  img: propTypes.string.isRequired,
+  name: propTypes.string.isRequired,
+  status: propTypes.string.isRequired,
+  statusText: propTypes.string.isRequired,
+};
 export default Contact;
